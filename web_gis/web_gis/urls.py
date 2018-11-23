8c772563a008509fdf5b3114eabd0a913a908bc6
Liridon Uka <liridonuka@Liridons-MacBook-Pro.local>
2018-11-23 22:20:36 +0100

    map application initialze

--- web_gis/web_gis/urls.py
+++ web_gis/web_gis/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map_app import views
 
 urlpatterns = [
+    path('',views.index, name='Home'),
     path('admin/', admin.site.urls),
 ]
