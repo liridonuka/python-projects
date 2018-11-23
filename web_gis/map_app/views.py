db34fe0e961d9d968cda47197ce4c6157465577d
Liridon Uka <liridonuka@Liridons-MacBook-Pro.local>
2018-11-23 22:33:37 +0100

    First index changed

--- web_gis/map_app/views.py
+++ web_gis/map_app/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello there')
+    list = []
+    for i in range(50):
+        list.append("Hello World ")
+
+    return HttpResponse(list)
