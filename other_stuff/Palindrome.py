f3d81da16253a85b0613f4031f8253c8d52df7c4
Liridon Uka <liridonuka@Liridons-MacBook-Pro.local>
2018-11-23 21:33:47 +0100

    other stuff initial

---
+++ other_stuff/Palindrome.py
@@ -0,0 +1,13 @@
+print("""--- This program is to check if the entered word is Palindrome or not. 
+If the word is palindrome it will return True otherwise you will be prompt
+to enter different word---
+..........................""")
+a = input("Enter some text here: ").lower()
+class Palindrome:
+	@staticmethod
+	def is_palindrome(word):
+		while word != word[::-1]:
+		    word = input("No! enter something different: ").lower()
+		return True
+print(Palindrome.is_palindrome(a))
+
---
+++ other_stuff/primitve_order_list.py
@@ -0,0 +1,14 @@
+list = []
+swapped = True
+num = int(input("How many elements do you want to sort? "))
+for i in range(num):
+    val = float(input("Enter next element: "))
+    list.append(val)
+while swapped:
+    swapped = False
+    for i in range(len(list) - 1):
+        if list[i] > list[i + 1]:
+            swapped = True
+            list[i],list[i + 1] = list[i + 1],list[i]
+print("Sorted:")
+print(list)
