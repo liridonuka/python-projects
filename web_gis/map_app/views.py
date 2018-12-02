99137b7e47087dc59a6fc105693e52b5823ade6c
Liridon Uka <liridonuka@Liridons-MacBook-Pro.local>
2018-12-02 22:15:45 +0100

    folium

--- web_gis/map_app/views.py
+++ web_gis/map_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from map_app.models import Komuna, Vendi
+
 # Create your views here.
 
 def index(request):
     result = Komuna.objects.order_by('id_komuna')
+    #result = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright", max_zoom = 4)
     komuna_dict = {'komuna_records':result}
     return render(request,'map_app/index.html',context=komuna_dict)
 
