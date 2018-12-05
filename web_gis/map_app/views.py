7a5909da6ed4ff9acfea352654029ffef5ce5984
Liridon Uka <liridonuka@Liridons-MacBook-Pro.local>
2018-12-05 13:26:58 +0100

    detail function done

--- web_gis/map_app/views.py
+++ web_gis/map_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from map_app.models import Komuna, Vendi
+import datetime
 
 # Create your views here.
-
 def index(request):
     result = Komuna.objects.order_by('id_komuna')
     #result = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright", max_zoom = 4)
@@ -11,5 +11,7 @@ def index(request):
     return render(request,'map_app/index.html',context=komuna_dict)
 
 def detail(request, question_id):
-    details={'insert_detail':'Te paraqitura ne detaje numri: ' + str(question_id)}
-    return render(request,'map_app/detail.html',context=details)
+    viti_aktual = str(datetime.date.today().year)
+    result = Komuna.objects.get(id_komuna = question_id)
+    komuna_dict = {'komuna_details': result}
+    return render(request,'map_app/details.html',komuna_dict)
